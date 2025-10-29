02a6fa7dd9269ac9b573a28be1fd5062fa35c186
Pal <mclydee.maliza@gmail.com>
2025-10-29 17:20:04 +0100

    Create manifest.js

---
+++ src/plugins/Light novel/manifest.js
@@ -0,0 +1,15 @@
+{
+  "id": "LN-reader",
+  "name": "Light Novel Reader",
+  "version": "1.0.0",
+  "description": "Allows reading light novels from NovelBuddy directly in Seanime for AniList novel entries.",
+  "author": "Pal",
+  "language": "typescript",
+  "type": "plugin",
+  "plugin": {
+    "version": "1",
+    "permissions": {
+      "scopes": ["anilist"]
+    }
+  }
+}
